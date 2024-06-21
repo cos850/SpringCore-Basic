893b9a98b5c8ecb94c71c57385ea924664d13de9
Hyeri Jeong <cos850@naver.com>
2024-06-21 15:21:29 +0900

    sec03 - spring 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,18 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // Spring은 ApplicationContext를 기반으로 동작. ApplicationContext룰 Spring 컨테이너라고 함.
+        // Annotation 기반의 AppConfig 설정 파일에 정보를 스프링 컨테이너에 등록하고 관리함
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // name은 method명 기준
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
